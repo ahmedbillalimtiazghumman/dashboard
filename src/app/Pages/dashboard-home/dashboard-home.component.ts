5e7a031696a658ddde3f605687367cd90a0ff3cf
ahmedbillalimtiazghumman <ahmedbillalimtiazghumman@gmail.com>
2021-05-03 10:08:05 -0700

    resolved issue in state management

--- src/app/Pages/dashboard-home/dashboard-home.component.ts
+++ src/app/Pages/dashboard-home/dashboard-home.component.ts
@@ -3,6 +3,8 @@ import { Store } from '@ngrx/store';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import * as fromRoot from '../../app-state/index';
+import * as fromAction from '../../app-state/actions/index';
+import { User } from 'src/app/app-state/entity';
 @Component({
   selector: 'app-dashboard-home',
   templateUrl: './dashboard-home.component.html',
@@ -10,13 +12,20 @@ import * as fromRoot from '../../app-state/index';
 })
 export class DashboardHomeComponent implements OnInit {
   list = ['test1', 'test2', 'test3'];
-  user: any;
+  user: Array<User>;
   destroy$: Subject<boolean> = new Subject<boolean>();
   constructor(private readonly store: Store) {
-    this.store
-      .select(fromRoot.getUsersSelector)
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((data) => (this.user = data.user));
   }
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.store
+    .select(fromRoot.getUsersSelector)
+    .pipe(takeUntil(this.destroy$))
+    .subscribe((data) => (this.user = data.user));
+    this.getUser()
+  }
+
+  getUser() {
+    this.store.dispatch(fromAction.getUsers());
+    console.log('User Data ==========>',this.user , typeof this.user);
+  }
 }
